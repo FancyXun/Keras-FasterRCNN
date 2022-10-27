d6e3c12a54d2b9a3ce0b1b0d3f2186324106603d
fancyxun <837633751@qq.com>
2022-10-27 15:56:29 +0800

    rpn loss 说明

--- keras_frcnn/losses.py
+++ keras_frcnn/losses.py
@@ -24,6 +24,7 @@ def rpn_loss_regr(num_anchors):
                 y_true[:, :4 * num_anchors, :, :] * (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / K.sum(
                 epsilon + y_true[:, :4 * num_anchors, :, :])
         else:
+            # smooth l1 loss
             x = y_true[:, :, :, 4 * num_anchors:] - y_pred
             x_abs = K.abs(x)
             x_bool = K.cast(K.less_equal(x_abs, 1.0), tf.float32)
@@ -38,6 +39,10 @@ def rpn_loss_regr(num_anchors):
 def rpn_loss_cls(num_anchors):
     def rpn_loss_cls_fixed_num(y_true, y_pred):
         if tf.keras.backend.image_data_format() == 'channels_last':
+            # y_true： TensorShape([1, 38, 53, 18])
+            # y_pred： TensorShape([1, 38, 53, 9])
+            # 注意：K.binary_crossentropy返回值的shape和输入一样，不会对最后一维度求mean
+            # 但是 tf.keras.losses.binary_crossentropy则会对最后一维度的交叉熵求mean
             return lambda_rpn_class * K.sum(
                 y_true[:, :, :, :num_anchors] *
                 K.binary_crossentropy(y_pred[:, :, :, :],
@@ -53,7 +58,6 @@ def rpn_loss_cls(num_anchors):
 
 def class_loss_regr(num_classes):
     def class_loss_regr_fixed_num(y_true, y_pred):
-        # print(y_true[:, :, 4*num_classes:], y_pred)
         y_true = K.cast(y_true, 'float32')
         x = y_true[:, :, 4 * num_classes:] - y_pred
         x_abs = K.abs(x)
